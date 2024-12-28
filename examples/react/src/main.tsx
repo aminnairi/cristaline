64f422bce8e14e6913f0f5583834bfdb0143515b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-28 19:33:21 +0100

    removed the strict mode for now until we find a way to cancel the initialize function

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,11 +5,9 @@ import { BrowserRouter } from "react-router"
 import { EventStoreProvider } from './eventstore'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <EventStoreProvider>
-        <App />
-      </EventStoreProvider>
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <EventStoreProvider>
+      <App />
+    </EventStoreProvider>
+  </BrowserRouter>
 )
