6b7fa1663ccc0c5276e525e0ce788b1e085c6734
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-28 16:17:52 +0100

    added a whitespace

--- packages/eventstore/index.ts
+++ packages/eventstore/index.ts
@@ -38,6 +38,7 @@ export interface Adapter<Event> {
 }
 
 export type EventStoreParser<Event> = (event: unknown) => Event | Error
+
 export interface CreateEventStoreOptions<State, Event> {
   readonly state: State,
   readonly parser: EventStoreParser<Event>,
